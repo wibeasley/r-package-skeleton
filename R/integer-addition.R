776b8acec04a787a241f0a1538809a1442a8bc85
Will Beasley <wibeasley@hotmail.com>
2017-05-02 10:23:29 -0500

    function that adds two integers
    closes #19

---
+++ R/integer-addition.R
@@ -0,0 +1,13 @@
+#' @name integer_addition
+#' @export
+#' @title adds two integers
+#' @description Adding two numbers
+#' @param a first integer
+#' @param b second integer
+
+integer_addition <- function(a,b)
+{
+  c <- a+b
+  return(c)
+
+}
