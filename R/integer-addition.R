c6fd4eff66bad9704db30d9e2d1e9d42c4557a87
Will Beasley <wibeasley@hotmail.com>
2017-05-02 22:15:15 -0500

    First basic function & test
    Closes #18

---
+++ R/integer-addition.R
@@ -0,0 +1,12 @@
+#' @name integer_addition
+#' @export
+#' @title adds two integers
+#' @description Adding two numbers
+#' @param a first integer
+#' @param b second integer
+
+integer_addition <- function( a, b )
+{
+  c <- a + b
+  return( c )
+}
